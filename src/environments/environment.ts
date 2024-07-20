d35814fafc064ef1a8838d9db279ae8a0b7338bc
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2024-07-20 20:14:29 +0300

    release 10

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  // api:"https://backend.qweira.com/api/",
-  // signalR:"https://backend.qweira.com/signalr",
+  api:"https://backend.qweira.com/api/",
+  signalR:"https://backend.qweira.com/signalr",
 
-  api:"http://173.212.222.155:5300/api/",
-  signalR:"http://173.212.222.155:5300/signalr",
+  // api:"http://173.212.222.155:5300/api/",
+  // signalR:"http://173.212.222.155:5300/signalr",
   secretKey:"a1d227f8-1749-4676-b41d-0d8c7605339c",
-  version:"V1.9.20"
+  version:"V1.10.0"
 };
